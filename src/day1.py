c926c00f16a64e56ecd2913fbe321dc2ab05ce33
João Victor Teófilo Salgado <jvictorteo2000@hotmail.com>
2023-12-10 20:52:34 -0300

    day1_part2

--- src/day1.py
+++ src/day1.py
@@ -1,9 +1,36 @@
+lookup = {
+    "one": "1",
+    "two": "2",
+    "three": "3",
+    "four": "4",
+    "five": "5",
+    "six": "6",
+    "seven": "7",
+    "eight": "8",
+    "nine": "9",
+    "zero": "0",
+}
+
+
+def find_number(line: str, is_reversed: bool = False):
+    buffer = ""
+    for character in line:
+        if is_reversed:
+            buffer = character + buffer
+        else:
+            buffer = buffer + character
+        if character.isdigit():
+            return character
+        if match := next((key for key in lookup.keys() if key in buffer), None):
+            return buffer.replace(buffer, lookup[match])
+
+
 def main(input_file_path: str):
     result: int = 0
     with open(input_file_path, "r", encoding="UTF-8") as input_file:
-        for line in input_file.readlines():
-            first_number = next(x for x in line if x.isdigit())
-            last_number = next(x for x in reversed(line) if x.isdigit())
+        for line in input_file.read().splitlines():
+            first_number = find_number(line)
+            last_number = find_number(line[::-1], True)
             current = int(f"{first_number}{last_number}")
             result = result + current
     return result
@@ -11,3 +38,4 @@ def main(input_file_path: str):
 
 if __name__ == "__main__":
     print(main("inputs/day1_input1.txt"))
+    print(main("inputs/day1_input2.txt"))
--- tests/day1_test.py
+++ tests/day1_test.py
@@ -6,3 +6,6 @@ from src.day1 import main
 class MainTest(unittest.TestCase):
     def test_sample1(self):
         self.assertEqual(main("inputs/day1_sample1.txt"), 142)
+
+    def test_sample2(self):
+        self.assertEqual(main("inputs/day1_sample2.txt"), 281)
