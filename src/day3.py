704ac904582cf6b12cfecf2ba0dc6048e48b3f7d
João Victor Teófilo Salgado <jvictorteo2000@hotmail.com>
2023-12-11 15:25:01 -0300

    day3_part2

--- src/day3.py
+++ src/day3.py
@@ -1,7 +1,8 @@
 import re
+from functools import reduce
 
 
-def get_neighbors(data: list[str], coordinates: list[tuple[int, int]]):
+def get_neighbors(max_y: int, max_x: int, coordinates: list[tuple[int, int]]):
     results: set[tuple[int, int]] = set([])
     for coordinate in coordinates:
         y = coordinate[0]
@@ -20,12 +21,12 @@ def get_neighbors(data: list[str], coordinates: list[tuple[int, int]]):
             neighbor_coordinate
             for neighbor_coordinate in neighbor_coordinates
             if neighbor_coordinate[0] >= 0
-            and neighbor_coordinate[0] < len(data)
+            and neighbor_coordinate[0] < max_y
             and neighbor_coordinate[1] >= 0
-            and neighbor_coordinate[1] < len(data[0])
+            and neighbor_coordinate[1] < max_x
         ]
         results = results.union(filtered_coordinates)
-    return [data[item[0]][item[1]] for item in results]
+    return results
 
 
 def part1(input_file_path: str):
@@ -42,7 +43,10 @@ def part1(input_file_path: str):
             candidate_coordinates = [
                 (index, item) for item in list(range(*candidate.span()))
             ]
-            candidate_neighbors = get_neighbors(data, candidate_coordinates)
+            candidate_neighbors = [
+                data[item[0]][item[1]]
+                for item in get_neighbors(len(data), len(line), candidate_coordinates)
+            ]
             is_candidate_valid = any(
                 bool(re.search(r"[^\d\.\n]", candidate_neighbor))
                 for candidate_neighbor in candidate_neighbors
@@ -52,5 +56,43 @@ def part1(input_file_path: str):
     return result
 
 
+def part2(input_file_path: str):
+    data: list[str] = []
+    with open(input_file_path, "r", encoding="UTF-8") as input_file:
+        for line in input_file.readlines():
+            data.append(line.rstrip())
+
+    result: int = 0
+
+    tokens: list[set[tuple[int, int]]] = []
+    for index, line in enumerate(data):
+        asterisks = re.finditer(r"(\*)", line)
+        for asterisk in asterisks:
+            token_coordinates = [
+                (index, item) for item in list(range(*asterisk.span()))
+            ]
+            token_neighbors = get_neighbors(len(data), len(line), token_coordinates)
+            tokens.append(token_neighbors)
+
+    gears: list[tuple[int, set[tuple[int, int]]]] = []
+    for index, line in enumerate(data):
+        digits = re.finditer(r"(\d+)", line)
+        for digit in digits:
+            digit_value = int(digit.group(1))
+            cdigit_coordinates = set(
+                [(index, item) for item in list(range(*digit.span()))]
+            )
+            gears.append((digit_value, cdigit_coordinates))
+
+    for token in tokens:
+        parts = [gear for gear in gears if token.intersection(gear[1])]
+        if len(parts) > 1:
+            ratio = reduce(lambda x, y: x * y, [part[0] for part in parts])
+            result = result + ratio
+
+    return result
+
+
 if __name__ == "__main__":
     print(part1("inputs/day3_input.txt"))
+    print(part2("inputs/day3_input.txt"))
--- tests/day3_test.py
+++ tests/day3_test.py
@@ -1,8 +1,11 @@
 import unittest
 
-from src.day3 import part1
+from src.day3 import part1, part2
 
 
 class MainTest(unittest.TestCase):
     def test_part1(self):
         self.assertEqual(part1("inputs/day3_sample1.txt"), 4361)
+
+    def test_part2(self):
+        self.assertEqual(part2("inputs/day3_sample1.txt"), 467835)
